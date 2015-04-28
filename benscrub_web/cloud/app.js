d0c7437ec448ebde723d7eac4d5bff1da0eb9e08
aru-imac <arunaharsa@gmail.com>
2015-04-29 00:37:19 +0800

    add gojek

--- benscrub_web/cloud/app.js
+++ benscrub_web/cloud/app.js
@@ -69,6 +69,201 @@ app.get('/h', function(req, res) {
 });
 
 
+app.get('/gojek/ready', function(req, res) {
+  var GojekAccount = Parse.Object.extend("gojek_account");
+  var query = new Parse.Query(GojekAccount);
+  query.exists("refer_to_code");
+  query.exists("refer_by_code");
+  query.notEqualTo("hide", true); // Hide completed appointments that doesnt have value
+  query.descending("createdAt");
+  query.find({
+    success: function(results) {
+      res.render('gojek', {data:JSON.parse(JSON.stringify(results))});
+    },
+    error: function(error) {
+      res.send(error);
+    }
+  });
+});
+
+app.get('/gojek/stash', function(req, res) {
+  var GojekAccount = Parse.Object.extend("gojek_account");
+  var query = new Parse.Query(GojekAccount);
+  query.descending("createdAt");
+  query.notEqualTo("hide", true);  // Hide completed appointments that doesnt have value
+  query.find({
+    success: function(results) {
+      res.render('gojek', {data:JSON.parse(JSON.stringify(results))});
+    },
+    error: function(error) {
+      res.send(error);
+    }
+  });
+});
+
+
+
+app.get('/api/gojek/create/:email', function(req, res) {
+  create_account(req.params.email, function(gojekAccount){
+    res.send(gojekAccount);
+  });
+});
+
+app.get('/api/gojek/:account_id/refer_to/:another_account_id', function(req, res) {
+  refer_account(req.params.account_id, req.params.parse_id, req.params.another_account_id, req.params.another_parse_id, function(response){
+    res.send(response);
+  })
+});
+
+app.get('/api/gojek/create', function(req, res, success_callback) {
+  create_account("budi15@mailinator.com", function(gojekAccount){
+    res.send(gojekAccount);
+  });
+});
+
+app.get('/api/gojek/create/:email/refer_to/:another_account_id', function(req, res, success_callback) { //Extending 'another_account_id' by creating email
+  create_account(req.params.email, function(newGojekAccount){
+    refer_account2(newGojekAccount, req.params.another_account_id, function(response){
+      res.send(response);
+    });
+  });
+});
+
+account_by_email = function(email, success_callback){
+  var GojekAccount = Parse.Object.extend("gojek_account");
+  var query = new Parse.Query(GojekAccount);
+  query.equalTo("email", email);
+  query.first({
+    success: function(gojekAccount) {
+      success_callback(gojekAccount);
+    }
+  });
+}
+
+account_by_account_id = function(account_id, success_callback){
+  var GojekAccount = Parse.Object.extend("gojek_account");
+  var query = new Parse.Query(GojekAccount);
+  query.equalTo("code", account_id);
+  query.first({
+    success: function(gojekAccount) {
+      success_callback(gojekAccount);
+    }
+  });
+}
+
+refer_account = function(account_id, refer_to_account_id, success_callback){
+  Parse.Cloud.httpRequest({
+    url: "http://api.gojek.co.id/gojek/customer/referral",
+    method: "POST",
+    headers: gojek_header_request(),
+    body: {
+      "referralId": account_id, //referralId is the code thats being inputted
+      "referrerId": refer_to_account_id, //referrerId will get the initial 50k
+    },
+    success: function(httpResponse) {
+      account_by_account_id(account_id, function(gojek_account){
+        refer_account2(gojek_account, refer_to_account_id, success_callback);
+      });
+    }
+  });
+
+}
+
+
+refer_account2 = function(gojek_account, refer_to_account_id, success_callback){
+  var account_id = gojek_account.get('code');
+  Parse.Cloud.httpRequest({
+    url: "http://api.gojek.co.id/gojek/customer/referral",
+    method: "POST",
+    headers: gojek_header_request(),
+    body: {
+      "referralId": account_id, //referralId is the code thats being inputted
+      "referrerId": refer_to_account_id, //referrerId will get the initial 50k
+    },
+    success: function(httpResponse) {
+      // success_callback(httpResponse);
+      gojek_account.set("refer_to_code", refer_to_account_id);
+      gojek_account.save(null, {
+        success: function(gojek_account){
+          var GojekAccount = Parse.Object.extend("gojek_account");
+          var query = new Parse.Query(GojekAccount);
+          query.equalTo("code", refer_to_account_id);
+          query.first({
+            success: function(referToGojekAccount) {
+              referToGojekAccount.set("refer_by_code", account_id);
+              referToGojekAccount.save(null, {
+                success: function(referToGojekAccount){
+                  success_callback(httpResponse);
+                }, error: function(error){
+                  success_callback(error);
+                }
+              });
+            }, error: function(error){
+              success_callback(error);
+            }
+          });
+        }, error: function(error){
+          success_callback(error);
+        }
+      });
+    }, error: function(error){
+      success_callback(error);
+    }
+  });
+
+}
+
+create_account = function(email, success_callback){
+
+  // Static Variable
+  var name = "Budi Sentosa";
+  var phone = Math.floor(Math.random() * 10000000);
+  var password = "111111111";
+
+  var postReqData = 
+  Parse.Cloud.httpRequest({
+    url: "http://api.gojek.co.id/gojek/customer/signup",
+    method: "POST",
+    headers: gojek_header_request(),
+    body: {
+      "email": email,
+      "name": name,
+      "phone": phone,
+      "password": password,
+      "confirmationPassword": password,
+    },
+    success: function(httpResponse) {
+
+      var GojekAccount = Parse.Object.extend("gojek_account");
+      var gojekAccount = new GojekAccount(); 
+      gojekAccount.set("email", email);
+      gojekAccount.set("name", name);
+      gojekAccount.set("code", JSON.stringify((JSON.parse(httpResponse.text)).customerId));
+      gojekAccount.save(null, {
+        success: function(gojekAccount) {
+          success_callback(gojekAccount);
+        }
+      });
+    },
+    error: function(httpResponse) {
+      // res.send(httpResponse.text)
+    }
+  });
+
+}
+
+gojek_header_request = function(){
+  return {
+      'Host': 'api.gojek.co.id',
+      'Connection': 'keep-alive',
+      'Accept': '*/*',
+      'User-Agent': 'GO-JEK/com.go-jek.ios (1.20; OS Version 8.1 (Build 12B411))',
+      'Content-Type': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.8,gl;q=0.6,id;q=0.4,ru;q=0.2,ms;q=0.2',
+      'Cookie': 'SERVERID=jetty-02b'
+    }
+}
+
 
 // Attach the Express app to Cloud Code.
 app.listen();
